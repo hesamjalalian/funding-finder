b9bc741014a003f46ccb971bf679733ec8205679
Hesam Jalalian <72898627+hesamjalalian@users.noreply.github.com>
2025-09-27 13:33:48 -0400

    Update app.py

--- app.py
+++ app.py
@@ -167,7 +167,7 @@ business_status = st.multiselect(
 )
 website = st.text_input("Organization Website (Optional)")
 
-st.subheader("Step 2: Describe Your Mission & Impact")
+st.subheader("Describe Your Mission & Impact")
 
 
 # education = st.text_input("Educational background of founder(s)")
